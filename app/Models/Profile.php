1eca3b63c74456a60be9054ae30b205931dbd5e1
Mo Khosh <mskhoshnazar@gmail.com>
2024-04-19 13:57:57 +0330

    register media collections

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Profile extends Model
+class Profile extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
+    public $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('avatar')
+            ->singleFile()
+            ->acceptsMimeTypes([
+                'image/jpeg',
+            ]);
+
+        $this->addMediaCollection('id_card')
+            ->singleFile()
+            ->acceptsMimeTypes([
+                'image/jpeg',
+            ]);
+
+        $this->addMediaCollection('certificates')
+            ->acceptsMimeTypes([
+                'image/jpeg',
+            ]);
+    }
 }
